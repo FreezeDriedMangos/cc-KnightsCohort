822b36b167f9c9723829f6da251f79fabf24bf78
clay <claytonharting+github@gmail.com>
2024-01-02 12:04:53 -0500

    added untracked file

---
+++ actions/ACharge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsCohort.actions
+{
+    public class ACharge : CardAction
+    {
+        public int distance = 0;
+
+        public override void Begin(G g, State s, Combat c)
+        {
+            var totalDist = c.otherShip.x - s.ship.x;
+            
+            c.QueueImmediate(new AMove() { dir = Math.Min(distance, Math.Abs(totalDist)) * Math.Sign(totalDist), targetPlayer = true });
+        }
+
+        public override Icon? GetIcon(State s)
+        {
+            return new Icon((Spr)MainManifest.sprites["icons/charge"].Id, distance, Colors.textMain);
+        }
+    }
+}
