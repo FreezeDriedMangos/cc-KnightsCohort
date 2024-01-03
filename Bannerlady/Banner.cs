162a55438094079fee75385869c1488d2ae807e5
clay <claytonharting+github@gmail.com>
2024-01-02 21:34:50 -0500

    fix: banners now function as intended - attacks actually pass through them instead of being copied from them. note that this also fixes tattered banners not working as a side effect

--- Bannerlady/Banner.cs
+++ Bannerlady/Banner.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using HarmonyLib;
+using Microsoft.Extensions.Logging;
 using Microsoft.Xna.Framework.Graphics;
+using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace KnightsCohort.Bannerlady
 {
+    [HarmonyPatch]
     public class Banner : StuffBase
     {
         public static readonly int MIDROW_SPRITE_WIDTH = 17;
@@ -11,29 +15,102 @@ namespace KnightsCohort.Bannerlady
         public static readonly double BANNER_ANIMATION_SPEED = 7;
         public override bool Invincible() { return true; }
 
-        public override List<CardAction>? GetActionsOnDestroyed(State s, Combat c, bool wasPlayer, int worldX)
+        //public override List<CardAction>? GetActionsOnDestroyed(State s, Combat c, bool wasPlayer, int worldX)
+        //{
+        //    return GetActionsOnShotWhileInvincible(s, c, wasPlayer, 0);
+        //}
+
+        //public override List<CardAction>? GetActionsOnShotWhileInvincible(State s, Combat c, bool wasPlayer, int damage)
+        //{
+        //    if (s.ship.Get((Status)MainManifest.statuses["shieldOfFaith"].Id) > 0 && !wasPlayer) return new() { };
+
+        //    if (c.currentCardAction is AAttack hit)
+        //    {
+        //        AAttack aattack = Mutil.DeepCopy(hit);
+
+        //        aattack.fromDroneX = this.x;
+        //        aattack.fast = true;
+
+        //        ModifyAction(aattack, s, c, wasPlayer);
+
+        //        //return new() { aattack };
+        //        return new() { };
+        //    }
+
+        //    MainManifest.Instance.Logger.LogError("Banner was shot with non attack " + c.currentCardAction?.GetType().FullName);
+        //    return new() { };
+        //}
+
+
+        public static int? AAttackGetFromX(AAttack a, State s, Combat c)
         {
-            return GetActionsOnShotWhileInvincible(s, c, wasPlayer, 0);
+            if (a.fromX.HasValue)
+            {
+                return a.fromX;
+            }
+            int num = (a.targetPlayer ? c.otherShip : s.ship).parts.FindIndex((Part p) => p.type == PType.cannon && p.active);
+            if (num != -1)
+            {
+                return num;
+            }
+            return null;
         }
 
-        public override List<CardAction>? GetActionsOnShotWhileInvincible(State s, Combat c, bool wasPlayer, int damage)
+        static StuffBase removedBanner = null;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(AAttack), nameof(AAttack.Begin))]
+        public static void RestoreNontatteredBanner(AAttack __instance, G g, State s, Combat c)
         {
-            if (s.ship.Get((Status)MainManifest.statuses["shieldOfFaith"].Id) > 0 && !wasPlayer) return new() { };
+            if (removedBanner == null) return;
+            c.stuff[removedBanner.x] = removedBanner;
+            removedBanner = null;
+        }
 
-            if (c.currentCardAction is AAttack hit)
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AAttack), nameof(AAttack.Begin))]
+        public static void BannerModifyAAttack(AAttack __instance, G g, State s, Combat c)
+        {
+            if (__instance.fromDroneX.HasValue) return;
+
+            Ship fromShip = (__instance.targetPlayer ? c.otherShip : s.ship);
+            var x = fromShip.x + Banner.AAttackGetFromX(__instance, s, c) ?? 0;
+
+            if (c.stuff.ContainsKey(x) && c.stuff[x] is Banner banner && s.ship.Get((Status)MainManifest.statuses["shieldOfFaith"].Id) <= 0)
             {
-                AAttack aattack = Mutil.DeepCopy(hit);
+                banner.ModifyAction(__instance, s, c, !__instance.targetPlayer);
 
-                aattack.fromDroneX = this.x;
-                aattack.fast = true;
+                if (!banner.Invincible())
+                {
+                    banner.DoDestroyedEffect(s, c);
+                    c.stuff.Remove(x);
 
-                ModifyAction(aattack, s, c, wasPlayer);
-
-                return new() { aattack };
+                    if (!__instance.targetPlayer)
+                    {
+                        foreach (Artifact artifact in s.EnumerateAllArtifacts())
+                        {
+                            artifact.OnPlayerDestroyDrone(s, c);
+                        }
+                    }
+                }
+                else
+                {
+                    // temporarily remove banner to make AAttack behave
+                    removedBanner = banner;
+                    c.stuff.Remove(x);
+                }
             }
-
-            MainManifest.Instance.Logger.LogError("Banner was shot with non attack " + c.currentCardAction?.GetType().FullName);
-            return new() { };
+        }
+        
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CombatUtils), nameof(CombatUtils.RaycastGlobal))]
+        public static void AttackPassthroughBannerPatch(RaycastResult __result, Combat c, Ship target, bool fromDrone, int worldX)
+        {
+            if (c.stuff.ContainsKey(worldX) && c.stuff[worldX] is Banner)
+            {
+                __result.hitShip = target.HasNonEmptyPartAtWorldX(worldX);
+                __result.hitDrone = false;
+            }
         }
 
         public virtual Spr GetSprite()
