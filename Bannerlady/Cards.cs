feaf41c2dcca930770c653bd567c994e656e9910
clay <claytonharting+github@gmail.com>
2024-01-11 11:26:59 -0500

    big redesign for bannerlady character - she no longer uses arrows and now leans much more heavily into evade and instant movement

--- Bannerlady/Cards.cs
+++ Bannerlady/Cards.cs
@@ -81,9 +81,26 @@ namespace KnightsCohort.Bannerlady.Cards
     {
         public override List<CardAction> GetActions(State s, Combat c)
         {
+            Guid stopId;
             return new()
             {
-               new ASpawn() { thing = new TatteredMartyrBanner() },
+                new ASpawn() { thing = new MartyrBanner() },
+                MainManifest.KokoroApi.ActionCosts.Make
+                (
+                    MainManifest.KokoroApi.ActionCosts.Cost
+                    (
+                        MainManifest.KokoroApi.ActionCosts.StatusResource
+                        (
+                            (Status)MainManifest.statuses["honor"].Id,
+                            Shockah.Kokoro.IKokoroApi.IActionCostApi.StatusResourceTarget.Player,
+                            (Spr)MainManifest.sprites["icons/honor_cost_unsatisfied"].Id,
+                            (Spr)MainManifest.sprites["icons/honor_cost"].Id
+                        ),
+                        amount: 2
+                    ),
+                    MainManifest.KokoroApi.Actions.MakeStop(out stopId)
+                ),
+                MainManifest.KokoroApi.Actions.MakeStopped(stopId, new AHurt(){ hurtAmount = 1, targetPlayer = true })
             };
         }
         public override CardData GetData(State state)
@@ -101,7 +118,8 @@ namespace KnightsCohort.Bannerlady.Cards
             {
                new ASpawn() { thing = new MercyBanner() },
                new AStatus() { status = Enum.Parse<Status>("droneShift"), statusAmount = 2, targetPlayer = true },
-               new AStatus() { status = Enum.Parse<Status>("shield"), statusAmount = 2, targetPlayer = true }
+               //new AStatus() { status = Enum.Parse<Status>("shield"), statusAmount = 2, targetPlayer = true }
+               new ARetreat() { distance = 2 }
             };
         }
         public override CardData GetData(State state)
@@ -118,8 +136,9 @@ namespace KnightsCohort.Bannerlady.Cards
             return new()
             {
                 new ACharge() { distance = 1 },
-                new AAttack() { damage = GetDmg(s, 1), targetPlayer = false },
                 new AStatus() { status = Enum.Parse<Status>("shield"), statusAmount = 1, targetPlayer = true },
+                new AAttack() { damage = GetDmg(s, 1), targetPlayer = false },
+                new ACharge() { distance = 1 },
             };
         }
         public override CardData GetData(State state)
@@ -187,12 +206,14 @@ namespace KnightsCohort.Bannerlady.Cards
         {
             return new()
             {
-               new ASpawn() { thing = new MartyrBanner() }
+               new ASpawn() { thing = new MartyrBanner(), offset = -1 },
+               new ASpawn() { thing = new MartyrBanner() },
+               new ASpawn() { thing = new MartyrBanner(), offset = 1 },
             };
         }
         public override CardData GetData(State state)
         {
-            return new() { cost = 2, exhaust = true };
+            return new() { cost = 3, exhaust = true };
         }
     }
 
@@ -224,7 +245,7 @@ namespace KnightsCohort.Bannerlady.Cards
             return new()
             {
                new AAttack() { damage = GetDmg(s, 1) },
-               new AMove() { dir = -2, targetPlayer = true },
+               new ARetreat() { distance = 2 },
             };
         }
         public override CardData GetData(State state)
@@ -257,6 +278,7 @@ namespace KnightsCohort.Bannerlady.Cards
             return new()
             {
                new ASpawn() { thing = new PirateBanner() },
+               new ARetreat() { distance = 2 }
             };
         }
         public override CardData GetData(State state)
@@ -288,7 +310,7 @@ namespace KnightsCohort.Bannerlady.Cards
         {
             return new()
             {
-               new ASpawn() { thing = new BannerOfInspiration() },
+               new ASpawn() { thing = new WarBanner() },
             };
         }
         public override CardData GetData(State state)
@@ -305,6 +327,7 @@ namespace KnightsCohort.Bannerlady.Cards
             return new()
             {
                new ASpawn() { thing = new BannerOfShielding() },
+               new ARetreat() { distance = 1 },
             };
         }
         public override CardData GetData(State state)
@@ -368,11 +391,23 @@ namespace KnightsCohort.Bannerlady.Cards
     {
         public override List<CardAction> GetActions(State s, Combat c)
         {
-            bool disabled = s.ship.Get((Status)MainManifest.statuses["honor"].Id) < 1;
             return new()
             {
-               new AStatus() { disabled = disabled, status = (Status)MainManifest.statuses["honor"].Id, targetPlayer = true, statusAmount = -1 },
-               new ADrawCard() { disabled = disabled, count = 3 },
+                MainManifest.KokoroApi.ActionCosts.Make
+                (
+                    MainManifest.KokoroApi.ActionCosts.Cost
+                    (
+                        MainManifest.KokoroApi.ActionCosts.StatusResource
+                        (
+                            (Status)MainManifest.statuses["honor"].Id,
+                            Shockah.Kokoro.IKokoroApi.IActionCostApi.StatusResourceTarget.Player,
+                            (Spr)MainManifest.sprites["icons/honor_cost_unsatisfied"].Id,
+                            (Spr)MainManifest.sprites["icons/honor_cost"].Id
+                        ),
+                        amount: 2
+                    ),
+                    new ADrawCard() { count = 3 }
+                ),
             };
         }
         public override CardData GetData(State state)
@@ -389,13 +424,128 @@ namespace KnightsCohort.Bannerlady.Cards
             return new()
             {
                 new AAttack() { damage = GetDmg(s, 1), targetPlayer = false },
-                new ACharge() { distance = 1 },
+                new ACharge() { distance = 2 },
                 new AAttack() { damage = GetDmg(s, 1), targetPlayer = false },
             };
         }
         public override CardData GetData(State state)
         {
             return new() { cost = 1 };
+        }
+    }
+
+    [CardMeta(rarity = Rarity.common, upgradesTo = new[] { Upgrade.A, Upgrade.B })]
+    public class BannerladyRally : Card
+    {
+        public override List<CardAction> GetActions(State s, Combat c)
+        {
+            CardAction upper =
+                MainManifest.KokoroApi.ActionCosts.Make
+                (
+                    MainManifest.KokoroApi.ActionCosts.Cost
+                    (
+                        MainManifest.KokoroApi.ActionCosts.StatusResource
+                        (
+                            Enum.Parse<Status>("droneShift"),
+                            Shockah.Kokoro.IKokoroApi.IActionCostApi.StatusResourceTarget.Player,
+                            (Spr)MainManifest.sprites["icons/honor_cost_unsatisfied"].Id,
+                            (Spr)MainManifest.sprites["icons/honor_cost"].Id
+                        ),
+                        amount: 2
+                    ),
+                    new AStatus() { status = Enum.Parse<Status>("evade"), statusAmount = 2, targetPlayer = true }
+                );
+            upper.disabled = this.flipped;
+
+            CardAction lower =
+                MainManifest.KokoroApi.ActionCosts.Make
+                (
+                    MainManifest.KokoroApi.ActionCosts.Cost
+                    (
+                        MainManifest.KokoroApi.ActionCosts.StatusResource
+                        (
+                            (Status)Enum.Parse<Status>("evade"),
+                            Shockah.Kokoro.IKokoroApi.IActionCostApi.StatusResourceTarget.Player,
+                            (Spr)MainManifest.sprites["icons/honor_cost_unsatisfied"].Id,
+                            (Spr)MainManifest.sprites["icons/honor_cost"].Id
+                        ),
+                        amount: 2
+                    ),
+                    new AStatus() { status = Enum.Parse<Status>("droneShift"), statusAmount = 2, targetPlayer = true }
+                );
+            upper.disabled = this.flipped;
+
+            return new()
+            {
+                upper,
+                new ADummyAction(),
+                new ADummyAction(),
+                lower,
+            };
+        }
+        public override CardData GetData(State state)
+        {
+            return new() { cost = 1, infinite = true };
+        }
+    }
+
+    [CardMeta(rarity = Rarity.common, upgradesTo = new[] { Upgrade.A, Upgrade.B })]
+    public class LiftedBurdens : Card
+    {
+        public override List<CardAction> GetActions(State s, Combat c)
+        {
+            return new()
+            {
+               new AVariableHint() { status = (Status)MainManifest.statuses["honor"].Id },
+               new AStatus() { status = Enum.Parse<Status>("evade"), statusAmount = s.ship.Get((Status)MainManifest.statuses["honor"].Id), xHint = 1 },
+            };
+        }
+        public override CardData GetData(State state)
+        {
+            return new() { cost = 1 };
+        }
+    }
+
+
+    [CardMeta(rarity = Rarity.common, upgradesTo = new[] { Upgrade.A, Upgrade.B })]
+    public class SharpEyes : Card
+    {
+        public int? GetDistance(State s)
+        {
+            if (s.route is not Combat c) return null;
+            if (c.stuff.Count == 0) return null;
+
+            int localCannonX = s.ship.parts.FindIndex((Part p) => p.type == PType.cannon && p.active);
+            if (localCannonX == -1) return null;
+            int cannonX = s.ship.x + localCannonX;
+
+            return c.stuff.Keys.Select(x => x - cannonX).OrderBy(x => Math.Abs(x)).First();
+        }
+        public override List<CardAction> GetActions(State s, Combat c)
+        {
+            int distance = GetDistance(s) ?? 0;
+            return new()
+            {
+               new AMove() { dir = distance, targetPlayer = true }
+            };
+        }
+        public override CardData GetData(State state)
+        {
+            string descr = "Align leftmost active cannon with nearest midrow object";
+            int cost = 1;
+            if (state.route is not Combat) return new() { cost = cost, description = $"{descr}." };
+
+
+            int? distance = GetDistance(state);
+            if (distance == null)
+            {
+                return new() { cost = cost, description = $"<c=3f3f3f>{descr}.</c>" };
+            }
+
+            string hint = $"(Right {distance})";
+            if (distance < 0) hint = $"(Left {-distance})";
+
+            return new() { cost = cost, description = $"{descr} {hint}." };
         }
     }
 }
