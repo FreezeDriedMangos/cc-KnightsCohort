a103e3a5b2dd6c9ef834f0a775272fe84de52917
clay <claytonharting+github@gmail.com>
2024-01-11 08:28:51 -0500

    updated tooltip dummy to account for aattack.gettooltips' side effects

--- actions/ATooltipDummy.cs
+++ actions/ATooltipDummy.cs
@@ -1,21 +1,123 @@
 ﻿using HarmonyLib;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KnightsCohort.actions
 {
     [HarmonyPatch(typeof(Card))]
     public class ATooltipDummy : ADummyAction
     {
+        public delegate void OnGetTooltips(State s);
+
+        public OnGetTooltips onGetTooltips;
         public List<Tooltip> tooltips;
         public List<Icon>? icons;
 
+        public static ATooltipDummy BuildStandIn(CardAction action, State s)
+        {
+            if (action is AAttack aattack)
+            {
+                return BuildFromAttack(aattack, s);
+            }
+
+            if (action is AStatus astatus)
+            {
+                return BuildFromStatus(astatus, s);
+            }
+
+            Icon? icon = action.GetIcon(s);
+            return new ATooltipDummy()
+            {
+                tooltips = action.GetTooltips(s),
+                icons = icon == null ? new() : new() { (Icon)action.GetIcon(s) }
+            };
+        }
+
+        public static ATooltipDummy BuildFromStatus(AStatus astatus, State s)
+        {
+            List<Icon> icons = new();
+
+            if (!astatus.targetPlayer)
+            {
+                icons.Add(new Icon(Enum.Parse<Spr>("icons_outgoing"), null, Colors.textMain));
+            }
+
+            Icon? icon = astatus.GetIcon(s);
+            if (icon != null)
+            {
+                icons.Add((Icon)icon);
+            }
+
+
+            return new ATooltipDummy()
+            {
+                tooltips = astatus.GetTooltips(s),
+                icons = icons
+            };
+        }
+
+        public static List<Tooltip> GetTooltipsNoSideEffects(AAttack aattack, State s)
+        {
+            var sshipparts = s.ship.parts;
+            s.ship.parts = new();
+            var c = s.route is Combat combat ? combat : null;
+            var cstuff = c != null ? c.stuff : null;
+            if (c != null) c.stuff = new();
+
+            var tooltips = aattack.GetTooltips(s);
+
+            if (c != null) c.stuff = cstuff;
+            s.ship.parts = sshipparts;
+
+            return tooltips;
+        }
+
+        public static ATooltipDummy BuildFromAttack(AAttack aattack, State s, bool hideOutgoingArrow = true)
+        {
+            List<Icon> icons = new();
+            var tooltips = GetTooltipsNoSideEffects(aattack, s);
+
+            icons.Add(new Icon(Enum.Parse<Spr>("icons_attack"), aattack.damage, Colors.redd));
+
+            if (aattack.stunEnemy)
+            {
+                icons.Add(new Icon(Enum.Parse<Spr>("icons_stun"), null, Colors.textMain));
+            }
+
+            if (aattack.status != null)
+            {
+                // this cast is ridiculous. It's needed because C# still thinks aattack.status can be null, even though it must be non-null to get here
+                var icon = new AStatus()
+                {
+                    status = (Status)aattack.status,
+                    targetPlayer = hideOutgoingArrow,
+                    statusAmount = aattack.statusAmount
+                }.GetIcon(s);
+
+                if (icon != null)
+                {
+                    if (!hideOutgoingArrow) icons.Add(new Icon(Enum.Parse<Spr>("icons_outgoing"), null, Colors.textMain));
+                    icons.Add((Icon)icon);
+                }
+            }
+
+            if (aattack.moveEnemy != 0)
+            {
+                Spr spr = aattack.moveEnemy < 0
+                    ? Enum.Parse<Spr>("icons_moveLeftEnemy")
+                    : Enum.Parse<Spr>("icons_moveRightEnemy");
+                icons.Add(new Icon(spr, Math.Abs(aattack.moveEnemy), Colors.redd));
+            }
+
+            return new ATooltipDummy()
+            {
+                tooltips = tooltips,
+                icons = icons,
+                onGetTooltips = (s) => aattack.GetTooltips(s)
+            };
+        }
+
         public override List<Tooltip> GetTooltips(State s)
         {
+            if (onGetTooltips != null) onGetTooltips(s);
             return tooltips ?? new();
         }
 
@@ -40,22 +142,25 @@ namespace KnightsCohort.actions
                     return false;
                 }
 
+                var iconNumberPadding = aTooltipDummy.icons.Count >= 3 ? 1 : 2;
+                var iconPadding = aTooltipDummy.icons.Count >= 3 ? 2 : 4;
+
                 Color spriteColor = (action.disabled ? Colors.disabledIconTint : new Color("ffffff"));
                 int w = 0;
                 bool isFirst = true;
 
                 foreach (var icon in aTooltipDummy.icons)
                 {
-                    IconAndOrNumber(icon.path, ref isFirst, ref w, g, action, state, spriteColor, true, amount: icon.number, iconWidth: SpriteLoader.Get(icon.path).Width);
+                    IconAndOrNumber(icon.path, ref isFirst, ref w, g, action, state, spriteColor, true, amount: icon.number, iconWidth: SpriteLoader.Get(icon.path).Width, iconNumberPadding: iconNumberPadding, iconPadding: iconPadding);
                 }
 
                 w = -w / 2;
                 isFirst = true;
                 foreach (var icon in aTooltipDummy.icons)
                 {
-                    IconAndOrNumber(icon.path, ref isFirst, ref w, g, action, state, spriteColor, dontDraw, amount: icon.number, iconWidth: SpriteLoader.Get(icon.path).Width, textColor: icon.color);
+                    IconAndOrNumber(icon.path, ref isFirst, ref w, g, action, state, spriteColor, dontDraw, amount: icon.number, iconWidth: SpriteLoader.Get(icon.path).Width, iconNumberPadding: iconNumberPadding, iconPadding: iconPadding, textColor: icon.color);
                 }
-            } 
+            }
             else
             {
                 return true;
@@ -64,11 +169,11 @@ namespace KnightsCohort.actions
             return false;
         }
 
-        private static void IconAndOrNumber(Spr icon, ref bool isFirst, ref int w, G g, CardAction action, State state, Color spriteColor, bool dontDraw, int iconNumberPadding = 2, int iconWidth = 8, int numberWidth = 6, int? amount = null, Color? textColor = null, bool flipY = false, int? x = null)
+        private static void IconAndOrNumber(Spr icon, ref bool isFirst, ref int w, G g, CardAction action, State state, Color spriteColor, bool dontDraw, int iconNumberPadding = 2, int iconWidth = 8, int numberWidth = 6, int? amount = null, Color? textColor = null, bool flipY = false, int? x = null, int iconPadding = 4)
         {
             if (!isFirst)
             {
-                w += 4;
+                w += iconPadding;
             }
             if (!dontDraw)
             {
