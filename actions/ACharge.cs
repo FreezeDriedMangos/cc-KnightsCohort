d471201175e81d73ea53ba76b4bd6afa779f3329
clay <claytonharting+github@gmail.com>
2024-01-11 11:28:35 -0500

    implemented ARetreat

--- actions/ACharge.cs
+++ actions/ACharge.cs
@@ -9,18 +9,42 @@ namespace KnightsCohort.actions
     public class ACharge : CardAction
     {
         public int distance = 0;
+        protected virtual string NonDirectionalSprite => "icons/charge";
+        protected virtual string DirectionalSprite => "icons/charge_directional";
 
         public override void Begin(G g, State s, Combat c)
         {
             var totalDist = c.otherShip.x - s.ship.x;
-            
+
             c.QueueImmediate(new AMove() { dir = Math.Min(distance, Math.Abs(totalDist)) * Math.Sign(totalDist), targetPlayer = true });
         }
 
         public override Icon? GetIcon(State s)
         {
-            // TODO: add a charge left and charge right and choose the appropriate icon depending on which direction the ship will actually move
-            return new Icon((Spr)MainManifest.sprites["icons/charge"].Id, distance, Colors.textMain);
+            // if (s.route is Combat c && ((s.routeOverride == null && c.routeOverride == null) || c.eyeballPeek))
+            if (s.route is Combat c)
+            {
+                var totalDist = c.otherShip.x - s.ship.x;
+                if (totalDist == 0) return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, distance, Colors.textMain);
+                if (totalDist < 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, distance, Colors.textMain);
+                if (totalDist > 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, distance, Colors.textMain, true);
+            }
+
+            return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, distance, Colors.textMain);
+        }
+    }
+
+    public class ARetreat : ACharge
+    {
+        protected override string NonDirectionalSprite => "icons/retreat";
+        protected override string DirectionalSprite => "icons/retreat_directional";
+
+        public override void Begin(G g, State s, Combat c)
+        {
+            var direction = -Math.Sign(c.otherShip.x - s.ship.x);
+            if (direction == 0) direction = 1;
+
+            c.QueueImmediate(new AMove() { dir = distance * direction, targetPlayer = true });
         }
     }
 }
