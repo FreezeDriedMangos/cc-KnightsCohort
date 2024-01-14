029d872abead4c91e0f602272f373feb5299888f
clay <claytonharting+github@gmail.com>
2024-01-13 20:46:46 -0500

    retreat now remembers the preferred direction at the start of playing a card

--- actions/ACharge.cs
+++ actions/ACharge.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,15 @@ namespace KnightsCohort.actions
         protected virtual string NonDirectionalSprite => "icons/charge";
         protected virtual string DirectionalSprite => "icons/charge_directional";
 
+        public static double GetCentralX(Ship ship)
+        {
+            return ship.x + ship.parts.Count / 2.0;
+        }
+
         public virtual int GetDir(State s, Combat c)
         {
             // TODO: calculate this from the middle part of each ship, not the leftmost (what ship.x represents)
-            var totalDist = c.otherShip.x - s.ship.x;
+            var totalDist = (int)Math.Ceiling(GetCentralX(c.otherShip) - GetCentralX(s.ship));
             return Math.Min(distance, Math.Abs(totalDist)) * Math.Sign(totalDist);
         }
 
@@ -41,15 +47,32 @@ namespace KnightsCohort.actions
         public override List<Tooltip> GetTooltips(State s) => new() { new TTGlossary(MainManifest.glossary["charge"].Head, distance) };
     }
 
+    [HarmonyPatch]
     public class ARetreat : ACharge
     {
+        public static int TiebreakerDirection = 1;
+        public int tiebreakerDirection = 0;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Combat), nameof(Combat.TryPlayCard))]
+        public static void TrackPlaying(Combat __instance, State s, Card card, bool playNoMatterWhatForFree = false, bool exhaustNoMatterWhat = false)
+        {
+            TiebreakerDirection = -Math.Sign(GetCentralX(__instance.otherShip) - GetCentralX(s.ship));
+            if (TiebreakerDirection == 0) TiebreakerDirection = 1;
+        }
+
+        public ARetreat() : base()
+        {
+            if (tiebreakerDirection == 0) tiebreakerDirection = TiebreakerDirection;
+        }
+
         protected override string NonDirectionalSprite => "icons/retreat";
         protected override string DirectionalSprite => "icons/retreat_directional";
 
         public override void Begin(G g, State s, Combat c)
         {
-            var direction = -Math.Sign(c.otherShip.x - s.ship.x);
-            if (direction == 0) direction = 1;
+            var direction = -Math.Sign(GetCentralX(c.otherShip) - GetCentralX(s.ship));
+            if (direction == 0) direction = tiebreakerDirection;
 
             c.QueueImmediate(new AMove() { dir = distance * direction, targetPlayer = true });
         }
@@ -61,8 +84,8 @@ namespace KnightsCohort.actions
             // if (s.route is Combat c && ((s.routeOverride == null && c.routeOverride == null) || c.eyeballPeek))
             if (s.route is Combat c)
             {
-                var totalDist = s.ship.x - c.otherShip.x;
-                if (totalDist < 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, distance, Colors.textMain);
+                var direction = -Math.Sign(GetCentralX(c.otherShip) - GetCentralX(s.ship));
+                if (direction < 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, distance, Colors.textMain);
                 else               return new Icon((Spr)MainManifest.sprites[DirectionalSprite + "_right"].Id, distance, Colors.textMain);
             }
 
