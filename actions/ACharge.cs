309125468f1e71cb9a7acb347fa9cdfa427c899b
clay <claytonharting+github@gmail.com>
2024-01-13 21:03:40 -0500

    ACharge and ARetreat now bake dir on init, as other actions do

--- actions/ACharge.cs
+++ actions/ACharge.cs
@@ -9,7 +9,7 @@ namespace KnightsCohort.actions
 {
     public class ACharge : CardAction
     {
-        public int distance = 0;
+        public int dir = 0;
         protected virtual string NonDirectionalSprite => "icons/charge";
         protected virtual string DirectionalSprite => "icons/charge_directional";
 
@@ -18,16 +18,17 @@ namespace KnightsCohort.actions
             return ship.x + ship.parts.Count / 2.0;
         }
 
-        public virtual int GetDir(State s, Combat c)
+        public static int GetDir(int distance, State s, Combat c)
         {
-            // TODO: calculate this from the middle part of each ship, not the leftmost (what ship.x represents)
+            if (s.route is not Combat) return distance;
+
             var totalDist = (int)Math.Ceiling(GetCentralX(c.otherShip) - GetCentralX(s.ship));
             return Math.Min(distance, Math.Abs(totalDist)) * Math.Sign(totalDist);
         }
 
         public override void Begin(G g, State s, Combat c)
         {
-            c.QueueImmediate(new AMove() { dir = GetDir(s, c), targetPlayer = true });
+            c.QueueImmediate(new AMove() { dir = dir, targetPlayer = true });
         }
 
         public override Icon? GetIcon(State s)
@@ -35,61 +36,50 @@ namespace KnightsCohort.actions
             // if (s.route is Combat c && ((s.routeOverride == null && c.routeOverride == null) || c.eyeballPeek))
             if (s.route is Combat c)
             {
-                var dir = GetDir(s, c);
-                if (dir == 0) return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, dir, Colors.textMain);
-                if (dir < 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, -dir, Colors.textMain);
-                if (dir > 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite+"_right"].Id, dir, Colors.textMain);
+                if (dir == 0) return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, Math.Abs(dir), Colors.textMain);
+                if (dir < 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, Math.Abs(dir), Colors.textMain);
+                if (dir > 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite+"_right"].Id, Math.Abs(dir), Colors.textMain);
             }
 
-            return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, distance, Colors.textMain);
+            return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, Math.Abs(dir), Colors.textMain);
         }
 
-        public override List<Tooltip> GetTooltips(State s) => new() { new TTGlossary(MainManifest.glossary["charge"].Head, distance) };
+        public override List<Tooltip> GetTooltips(State s) => new() { new TTGlossary(MainManifest.glossary["charge"].Head, Math.Abs(dir)) };
     }
 
-    [HarmonyPatch]
     public class ARetreat : ACharge
     {
-        public static int TiebreakerDirection = 1;
-        public int tiebreakerDirection = 0;
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(Combat), nameof(Combat.TryPlayCard))]
-        public static void TrackPlaying(Combat __instance, State s, Card card, bool playNoMatterWhatForFree = false, bool exhaustNoMatterWhat = false)
-        {
-            TiebreakerDirection = -Math.Sign(GetCentralX(__instance.otherShip) - GetCentralX(s.ship));
-            if (TiebreakerDirection == 0) TiebreakerDirection = 1;
-        }
-
-        public ARetreat() : base()
-        {
-            if (tiebreakerDirection == 0) tiebreakerDirection = TiebreakerDirection;
-        }
-
         protected override string NonDirectionalSprite => "icons/retreat";
         protected override string DirectionalSprite => "icons/retreat_directional";
 
-        public override void Begin(G g, State s, Combat c)
-        {
-            var direction = -Math.Sign(GetCentralX(c.otherShip) - GetCentralX(s.ship));
-            if (direction == 0) direction = tiebreakerDirection;
 
-            c.QueueImmediate(new AMove() { dir = distance * direction, targetPlayer = true });
+        public static new int GetDir(int distance, State s, Combat c)
+        {
+            if (s.route is not Combat) return distance;
+
+            var totalDist = -(int)Math.Ceiling(GetCentralX(c.otherShip) - GetCentralX(s.ship));
+            if (totalDist == 0) totalDist = 1;
+            return distance * Math.Sign(totalDist);
         }
 
-        public override List<Tooltip> GetTooltips(State s) => new() { new TTGlossary(MainManifest.glossary["retreat"].Head, distance) };
+        public override void Begin(G g, State s, Combat c)
+        {
+            c.QueueImmediate(new AMove() { dir = dir, targetPlayer = true });
+        }
+
+        public override List<Tooltip> GetTooltips(State s) => new() { new TTGlossary(MainManifest.glossary["retreat"].Head, Math.Abs(dir)) };
 
         public override Icon? GetIcon(State s)
         {
             // if (s.route is Combat c && ((s.routeOverride == null && c.routeOverride == null) || c.eyeballPeek))
             if (s.route is Combat c)
             {
-                var direction = -Math.Sign(GetCentralX(c.otherShip) - GetCentralX(s.ship));
-                if (direction < 0) return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, distance, Colors.textMain);
-                else               return new Icon((Spr)MainManifest.sprites[DirectionalSprite + "_right"].Id, distance, Colors.textMain);
+                if (dir == 0) return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, Math.Abs(dir), Colors.textMain);
+                if (dir < 0)  return new Icon((Spr)MainManifest.sprites[DirectionalSprite].Id, Math.Abs(dir), Colors.textMain);
+                if (dir > 0)  return new Icon((Spr)MainManifest.sprites[DirectionalSprite + "_right"].Id, Math.Abs(dir), Colors.textMain);
             }
 
-            return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, distance, Colors.textMain);
+            return new Icon((Spr)MainManifest.sprites[NonDirectionalSprite].Id, Math.Abs(dir), Colors.textMain);
         }
     }
 }
